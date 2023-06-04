dbd93c883a2b92b4a83cb70c766561e9d3549de5
Felipe Marinho <felipmarinho@gmail.com>
2023-06-03 21:56:31 -0300

    update

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,7 +1,12 @@
+import NextLink from "next/link";
+
 export default function AboutScreen() {
   return (
     <div>
       <h1>sobre</h1>
+      <NextLink href="/">
+        <a>Ir para a Home</a>
+      </NextLink>
     </div>
   );
 }
