e74ba62f99832bef28995388b4cebc38a9ff6686
Felipe Marinho <felipmarinho@gmail.com>
2023-06-04 18:30:16 -0300

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -21,14 +21,14 @@ const GH_COMMENT = `
 - Deploy URL: [${DEPLOY_URL}] (${DEPLOY_URL})
 `;
 
+console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
+console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
+
 const defaultHeaders = {};
 defaultHeaders["authorization"] = `token ${GITHUB_TOKEN}`;
 defaultHeaders["accept"] =
   "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
 defaultHeaders["content-type"] = "application/json";
-
-console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
-console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
 
 fetch(
   `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
