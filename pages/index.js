db4395b61955f0ba16f49303fd9941077a8cfe29
Felipe Marinho <felipmarinho@gmail.com>
2023-06-05 17:34:36 -0300

    test lint

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import NextLink from "next/link";
 
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Página inicial [Nova alteração]</h1>
-      <NextLink href="/sobre">
-        <a>Ir para a sobre</a>
-      </NextLink>
-    </div>
-  );
+<div>
+<h1>Página inicial [Nova alteração]</h1>
+<NextLink href="/sobre">
+<a>Ir para a sobre</a>
+</NextLink>
+</div>
+);
 }
